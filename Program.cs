c0e11f8db617837e9cd6f67e1b8c0ae98dafaf23
Anton Gadyatskiy <aoagadbart90@gmail.com>
2022-12-17 19:50:19 +0300

    Сделал первую задачу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+
+int Number (int num)
+{
+    int sot = num/10 % 10;
+    return sot;
+}
+
+
+int randomNumber = new Random().Next(100,1000);
+Console.WriteLine(randomNumber);
+int result = Number(randomNumber);
+Console.WriteLine(result);
+
