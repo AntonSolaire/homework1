61dc95a6ee4fd5ac941b03eedd6a10954601aa1d
Anton Gadyatskiy <aoagadbart90@gmail.com>
2022-12-17 20:21:29 +0300

    Не дописал вторую, перехожу к третьей

--- Program.cs
+++ Program.cs
@@ -5,16 +5,46 @@
 // 918 -> 1
 
 
+// int Number (int num)
+// {
+//     int sot = num/10 % 10;
+//     return sot;
+// }
 
-int Number (int num)
+// int randomNumber = new Random().Next(100,1000);
+// Console.WriteLine(randomNumber);
+// int result = Number(randomNumber);
+// Console.WriteLine(result);
+
+
+
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+void ThirdNumber (int num1)
 {
-    int sot = num/10 % 10;
-    return sot;
+    if (num1<100){
+        Console.WriteLine ($" У {num1} нет третьей цифры ");
+    }
+    else {
+        if (num1>100 && num1<1000){
+        int sot = num1%10;
+        Console.WriteLine ($"Третья цифра в числе {num1} это {sot}");
+        }
+    }
+  
 }
 
+Console.WriteLine ("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine ());
+ThirdNumber(num1);
 
-int randomNumber = new Random().Next(100,1000);
-Console.WriteLine(randomNumber);
-int result = Number(randomNumber);
-Console.WriteLine(result);
+
 
